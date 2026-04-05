9b722ed70ac76e79f5afe1f39381aba3efca8abb
ahmedkhan <ahmedkhanofficials@gmail.com>
2026-04-05 07:07:28 +0500

    fixed cors issuse

--- Server/server.js
+++ Server/server.js
@@ -268,7 +268,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('/*', cors(corsOptions));
 
 // Import routes
 const authRoutes = require('./routes/Authroutes');
